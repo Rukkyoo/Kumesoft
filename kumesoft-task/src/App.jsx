4aebc4ad9c5d05a92dd4d9582f753007de83f837
Michael Omonedo <rukkyomonedo@gmail.com>
2025-04-13 15:17:19 +0100

    initial push

---
+++ kumesoft-task/src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from "./home/homePage.jsx"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ kumesoft-task/src/home/homePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const homePage = () => {
+  return (
+    <div className='bg-red-500 text-amber-500'>homePage</div>
+  )
+}
+
+export default homePage
